047dcdb6a21c1c3fd73abf1f6f64f05f0cc35148
Craig McLeod <craig.mcleod@2itesting.com>
2022-11-26 16:50:51 +0000

    Push from local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import * as Papa from "papaparse";
+import ornament from "./Card/CardAssets/ornament.jpg";
+import pokemon from "./Card/CardAssets/poke.jpg";
+import code from "./Card/CardAssets/code.jpg";
+import amongus from "./Card/CardAssets/amongus.jpg";
+import chewy from "./Card/CardAssets/chewy.jpg";
+import diehard from "./Card/CardAssets/diehard.jpg";
+import santa from "./Card/CardAssets/santa.jpeg";
+import overcooked from "./Card/CardAssets/overcooked.jpg";
+import halo from "./Card/CardAssets/halo.jpg";
+import mario from "./Card/CardAssets/mario.jpg";
+import lights from "./fairy-lights.jpg"
+import Card from "./Card/card";
+const shuffle = "WzQsMTUsMTcsMjYsOSwzMCwxMywxNiwxMiwyM11=";
+const days = [5, 8, 14, 6, 7, 2, 9, 12, 13, 1, 16, 15];
+
+const pic = [
+  ornament,
+  pokemon,
+  code,
+  amongus,
+  chewy,
+  diehard,
+  santa,
+  overcooked,
+  halo,
+  mario,
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [days1, setDays] = useState();
+  
+  useEffect(() => {
+    fetch("./Book.csv")
+      .then((csv) => csv.text())
+      .then((resText) => {
+        const parsed = Papa.parse(resText);
+        const newObj = [];
+        for (let i = 1; i < parsed.data.length; i++) {
+          newObj.push({ challenge: parsed.data[i][0], by: parsed.data[i][1] });
+        }
+        setDays(newObj);
+      });
+  }, []);
+
+  if (!days1) {
+    return "loading";
+  }
+  if (days1)
+    return (
+      <div className="App">
+        <div id="advent-calendar" style={{backgroundImage: `url("${lights}")`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+          {days1
+            .filter((el, i) => {
+              return JSON.parse(atob(shuffle)).includes(i + 1);
+            })
+            .map((x, i) => (
+              <Card 
+                days={days}
+                pic={pic}
+                i={i}
+                currentCard={x}
+              />
+            ))}
+        </div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/Card/card.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+const Card = ({days, pic, i, currentCard}) => {
+    const month = 11
+    return(
+        <div
+        className={`advent-card ${days[i] <= new Date().getDate() &&
+          new Date().getMonth() === month ? '' : 'closed'}`}
+        style={{
+          backgroundImage:
+            days[i] <= new Date().getDate() &&
+            new Date().getMonth() === month
+              ? "none"
+              : `url("${pic[i]}")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundColor:
+            days[i] <= new Date().getDate() &&
+            new Date().getMonth() === month
+              ? "black"
+              : "none",
+          color:
+            days[i] <= new Date().getDate() &&
+            new Date().getMonth() === month
+              ? "green"
+              : "rgba(178, 65, 0, 1)"
+        }}
+      >
+        <p>
+          {days[i] <= new Date().getDate() &&
+          new Date().getMonth() === month
+            ? currentCard.challenge + ' - ' + currentCard.by 
+            : days[i]}
+        </p>
+      </div>
+    )
+}
+export default Card
