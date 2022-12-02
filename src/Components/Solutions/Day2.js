69549521c7862675a8d16c38020c29304b2aca39
Tomasz Tracz <tomasz.tracz@2itesting.com>
2022-12-02 13:34:11 +0000

    Reroll with click

--- src/Components/Solutions/Day2.js
+++ src/Components/Solutions/Day2.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import "./Day2.css";
 const Day2 = () => {
+  const [dice1, setDice1] = useState(Math.floor(Math.random() * 6 + 1));
+  const [dice2, setDice2] = useState(Math.floor(Math.random() * 6 + 1));
+
+  const rollDice = () => {
+    setDice1(Math.floor(Math.random() * 6 + 1));
+    setDice2(Math.floor(Math.random() * 6 + 1));
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", rollDice);
+    return () => {
+      window.removeEventListener("click", rollDice);
+    };
+  });
+
   return (
     <div className="advent-card">
       <div>
         <h1 className="text-3xl">Day 2</h1>
         <p>Pair of rollin' dice</p>
         <div className="flex">
-          <CssCube show={Math.floor(Math.random() * 6 + 1)} />
-          <CssCube show={Math.floor(Math.random() * 6 + 1)} />
+          <CssCube show={dice1} />
+          <CssCube show={dice2} />
         </div>
       </div>
     </div>
