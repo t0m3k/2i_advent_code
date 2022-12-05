1baeebb07fd73f6d2260323e857ffdc5c5f4a391
Tomasz Tracz <tomasz.tracz@2itesting.com>
2022-12-05 09:26:08 +0000

    Day3

---
+++ src/Components/Solutions/Day3.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Day3 = () => {
+  return (
+    <div className="advent-card">
+      <div>
+        <h1 className="text-3xl">Day 3</h1>
+        <p className="text-lg">
+          <span className="text-blue-600">throw new</span>{" "}
+          NullPointerException();
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Day3;
--- src/Components/Solutions/index.js
+++ src/Components/Solutions/index.js
@@ -1,6 +1,7 @@
 import Day1 from "./Day1";
 import Day2 from "./Day2";
+import Day3 from "./Day3";
 
-const Solutions = [Day1, Day2];
+const Solutions = [Day1, Day2, null, null, Day3];
 
 export default Solutions;
